b82543adfba1fe3471f62a3cec8dcc5a4bc88669
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-12 20:20:26 +0300

    refactor: web.xml

--- src/main/java/dao/entity/Department.java
+++ src/main/java/dao/entity/Department.java
@@ -14,4 +14,62 @@ public class Department {
     private String phone;
     @ManyToOne
     private Location location;
+
+    public Department() {
+    }
+
+    public Department(String name, Department parent, String phone, Location location) {
+        this.name = name;
+        this.parent = parent;
+        this.phone = phone;
+        this.location = location;
+    }
+
+    public Department(Long id, String name, Department parent, String phone, Location location) {
+        this.id = id;
+        this.name = name;
+        this.parent = parent;
+        this.phone = phone;
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getParent() {
+        return parent;
+    }
+
+    public void setParent(Department parent) {
+        this.parent = parent;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
 }
