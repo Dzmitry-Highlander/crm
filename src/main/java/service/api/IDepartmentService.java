bcd9fd8926c2db5c49a905bfb4fa17fcec08ac6c
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-13 12:16:16 +0300

    add: IDepartmentService.java

---
+++ src/main/java/service/api/IDepartmentService.java
@@ -0,0 +1,7 @@
+package service.api;
+
+import core.dto.DepartmentCreateUpdateDTO;
+import core.dto.DepartmentDTO;
+
+public interface IDepartmentService extends IServiceCRUD<DepartmentCreateUpdateDTO, DepartmentDTO> {
+}
