a8668424ad62398997bf69c7f952c536c264b524
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-14 02:25:27 +0300

    refactor: LocationJDBCDao.java

--- src/main/java/dao/db/LocationJDBCDao.java
+++ src/main/java/dao/db/LocationJDBCDao.java
@@ -5,7 +5,10 @@ import dao.entity.Location;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.CriteriaUpdate;
 import jakarta.persistence.criteria.Root;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import service.util.HibernateUtil;
 
 import java.util.List;
@@ -13,7 +16,22 @@ import java.util.List;
 public class LocationJDBCDao implements ILocationDao {
     @Override
     public Location create(Location item) {
-        return null;
+        EntityManager em = HibernateUtil.getEntityManager();
+        Session session = em.unwrap(Session.class);
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaUpdate<Location> criteriaUpdate = cb.createCriteriaUpdate(Location.class);
+        Root<Location> root = criteriaUpdate.from(Location.class);
+
+        criteriaUpdate.set("id", item.getId());
+        criteriaUpdate.set("name", item.getName());
+        criteriaUpdate.where(cb.equal(root.get("id"), item.getId()));
+        criteriaUpdate.where(cb.equal(root.get("name"), item.getName()));
+
+        Transaction transaction = session.beginTransaction();
+        session.createSelectionQuery(criteriaUpdate.toString());
+        transaction.commit();
+
+        return item;
     }
 
     @Override
