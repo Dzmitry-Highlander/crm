62b5adbb2057eb974bf687ee16c1d308658d78f1
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-13 12:22:07 +0300

    add: DepartmentService.java

---
+++ src/main/java/service/DepartmentService.java
@@ -0,0 +1,34 @@
+package service;
+
+import core.dto.DepartmentCreateUpdateDTO;
+import core.dto.DepartmentDTO;
+import service.api.IDepartmentService;
+
+import java.util.List;
+
+public class DepartmentService implements IDepartmentService {
+    @Override
+    public DepartmentDTO create(DepartmentCreateUpdateDTO item) {
+        return null;
+    }
+
+    @Override
+    public DepartmentDTO read(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<DepartmentDTO> read() {
+        return null;
+    }
+
+    @Override
+    public DepartmentDTO update(DepartmentCreateUpdateDTO item) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
