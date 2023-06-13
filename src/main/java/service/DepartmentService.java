3399c0c52f0c4d99641e9e5171986c99b557fd7c
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-13 12:31:56 +0300

    refactor: DepartmentService.java, pom.xml

--- src/main/java/service/DepartmentService.java
+++ src/main/java/service/DepartmentService.java
@@ -2,6 +2,10 @@ package service;
 
 import core.dto.DepartmentCreateUpdateDTO;
 import core.dto.DepartmentDTO;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import service.api.IDepartmentService;
 
 import java.util.List;
@@ -9,6 +13,12 @@ import java.util.List;
 public class DepartmentService implements IDepartmentService {
     @Override
     public DepartmentDTO create(DepartmentCreateUpdateDTO item) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("crm");
+
+        EntityManager em = factory.createEntityManager();
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+
         return null;
     }
 
