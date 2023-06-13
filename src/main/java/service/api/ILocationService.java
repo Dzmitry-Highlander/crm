34531c3a0aee8e6a79b202a0a4bdc1b005dca809
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-13 12:17:41 +0300

    add: ILocationService.java

---
+++ src/main/java/service/api/ILocationService.java
@@ -0,0 +1,7 @@
+package service.api;
+
+import core.dto.LocationCreateUpdateDTO;
+import core.dto.LocationDTO;
+
+public interface ILocationService extends IServiceCRUD<LocationCreateUpdateDTO, LocationDTO> {
+}
