52ff0005259e66de7facca9442bf87d11b027d99
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-13 12:13:22 +0300

    refactor: IServiceCRUD.java

--- src/main/java/service/api/IServiceCRUD.java
+++ src/main/java/service/api/IServiceCRUD.java
@@ -1,4 +1,15 @@
 package service.api;
 
-public interface IServiceCRUD {
+import java.util.List;
+
+public interface IServiceCRUD<T, S> {
+    S create(T item);
+
+    S get(Long id);
+
+    List<S> get();
+
+    S update(T item);
+
+    void delete(Long id);
 }
