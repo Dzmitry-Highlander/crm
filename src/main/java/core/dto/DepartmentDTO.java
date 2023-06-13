f71ede954d79f4433fae1c549bd291b8488887ba
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-13 12:02:49 +0300

    refactor: DepartmentDTO.java

--- src/main/java/core/dto/DepartmentDTO.java
+++ src/main/java/core/dto/DepartmentDTO.java
@@ -11,4 +11,55 @@ public class DepartmentDTO {
     private Department parent;
     private String phone;
     private Location location;
+
+    public DepartmentDTO() {
+    }
+
+    public DepartmentDTO(Long id, String name, Department parent, String phone, Location location) {
+        this.id = id;
+        this.name = name;
+        this.parent = parent;
+        this.phone = phone;
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getParent() {
+        return parent;
+    }
+
+    public void setParent(Department parent) {
+        this.parent = parent;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
 }
