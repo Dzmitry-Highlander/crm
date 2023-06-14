0ac7ac12927b14bfaf7010129c83e6bf84b47d23
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-14 08:42:02 +0300

    add: LocationCreateServlet.java

---
+++ src/main/java/endpoints/web/LocationCreateServlet.java
@@ -0,0 +1,37 @@
+package endpoints.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import core.dto.LocationCreateUpdateDTO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import service.api.ILocationService;
+import service.factory.LocationServiceFactory;
+import service.factory.ObjectMapperFactory;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/api/location/create")
+public class LocationCreateServlet extends HttpServlet {
+    private final ILocationService locationService;
+    private final ObjectMapper objectMapper;
+
+    public LocationCreateServlet() {
+        this.locationService = LocationServiceFactory.getInstance();
+        this.objectMapper = ObjectMapperFactory.getInstance();
+        this.objectMapper.findAndRegisterModules();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+
+        PrintWriter writer = resp.getWriter();
+
+        writer.write(objectMapper.writeValueAsString(locationService
+                .create(new LocationCreateUpdateDTO("Брест, Беларусь"))));
+    }
+}
