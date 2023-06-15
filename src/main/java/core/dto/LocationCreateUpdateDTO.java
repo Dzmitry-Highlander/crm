7a2fe6ffcb62a5b1b248ced964d2a334b5fea842
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-15 10:35:36 +0300

    refactor: LocationCreateUpdateDTO.java

--- src/main/java/core/dto/LocationCreateUpdateDTO.java
+++ src/main/java/core/dto/LocationCreateUpdateDTO.java
@@ -3,10 +3,17 @@ package core.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class LocationCreateUpdateDTO {
+    @JsonProperty("id")
+    private Long id;
     @JsonProperty("name")
     private String name;
 
     public LocationCreateUpdateDTO() {
+    }
+
+    public LocationCreateUpdateDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public LocationCreateUpdateDTO(String name) {
