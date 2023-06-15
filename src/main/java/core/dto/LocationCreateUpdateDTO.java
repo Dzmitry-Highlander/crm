ed812678d2dd08d9511be7e6e68acc92fd259c2f
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-15 10:48:30 +0300

    refactor: LocationCreateUpdateDTO.java

--- src/main/java/core/dto/LocationCreateUpdateDTO.java
+++ src/main/java/core/dto/LocationCreateUpdateDTO.java
@@ -20,6 +20,14 @@ public class LocationCreateUpdateDTO {
         this.name = name;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
