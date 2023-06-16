c59e7e7f5bff45512b0aa0c56e7914d0f4ea84fc
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-16 20:50:41 +0300

    refactor: DepartmentConverterUtilFactory.java, ILocationService.java, LocationConverterUtilFactory.java

--- src/main/java/service/api/ILocationService.java
+++ src/main/java/service/api/ILocationService.java
@@ -1,7 +1,6 @@
 package service.api;
 
 import core.dto.LocationCreateUpdateDTO;
-import core.dto.LocationDTO;
 import dao.entity.Location;
 
 public interface ILocationService extends IServiceCRUD<Location, LocationCreateUpdateDTO> {
