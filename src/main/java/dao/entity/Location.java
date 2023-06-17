98ba395e8881d0a49d2fbad30e5244d6b35ee13b
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-17 11:28:51 +0300

    refactor: 0_init_sql, Location.java

--- src/main/java/dao/entity/Location.java
+++ src/main/java/dao/entity/Location.java
@@ -14,17 +14,25 @@ public class Location implements Serializable {
     private Long id;
     @Column(name = "name")
     private String name;
+    @Column(name = "version")
+    private Long version;
 
     public Location() {
     }
 
-    public Location(Long id, String name) {
-        this.id = id;
+    public Location(String name) {
         this.name = name;
     }
 
-    public Location(String name) {
+    public Location(String name, Long version) {
         this.name = name;
+        this.version = version;
+    }
+
+    public Location(Long id, String name, Long version) {
+        this.id = id;
+        this.name = name;
+        this.version = version;
     }
 
     public Long getId() {
@@ -43,16 +51,24 @@ public class Location implements Serializable {
         this.name = name;
     }
 
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Location location = (Location) o;
-        return Objects.equals(id, location.id) && Objects.equals(name, location.name);
+        if (!(o instanceof Location location)) return false;
+        return Objects.equals(getId(), location.getId()) && Objects.equals(getName(),
+                location.getName()) && Objects.equals(getVersion(), location.getVersion());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        return Objects.hash(getId(), getName(), getVersion());
     }
 }
