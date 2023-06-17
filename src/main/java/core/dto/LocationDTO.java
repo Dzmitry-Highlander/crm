a53a1f495547375731d3b28b421e5da45009a3ec
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-17 12:14:47 +0300

    refactor: LocationDTO.java

--- src/main/java/core/dto/LocationDTO.java
+++ src/main/java/core/dto/LocationDTO.java
@@ -1,17 +1,21 @@
 package core.dto;
 
+import java.time.LocalDateTime;
+
 public class LocationDTO {
     private Long id;
     private String name;
-    private Long version;
+    private LocalDateTime version;
+    private LocalDateTime createDate;
 
     public LocationDTO() {
     }
 
-    public LocationDTO(Long id, String name, Long version) {
+    public LocationDTO(Long id, String name, LocalDateTime version, LocalDateTime createDate) {
         this.id = id;
         this.name = name;
         this.version = version;
+        this.createDate = createDate;
     }
 
     public Long getId() {
@@ -30,11 +34,19 @@ public class LocationDTO {
         this.name = name;
     }
 
-    public Long getVersion() {
+    public LocalDateTime getVersion() {
         return version;
     }
 
-    public void setVersion(Long version) {
+    public void setVersion(LocalDateTime version) {
         this.version = version;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
     }
 }
